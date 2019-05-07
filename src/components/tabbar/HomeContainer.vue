dec1cd2b19e93f16e0cc5a4ff0626010c2d19ad0
Yhungry <wangyonghan9872@163.com>
2019-05-07 10:28:24 +0800

    轮播图完成

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,18 +1,43 @@
 <template>
-  <div>Home----</div>
+  <div>
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="(item,i) in Lunbolist" :key="i">
+        <img :src="item.img">
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      Lunbolist: []
+    };
   },
-  created() {},
-  methods: {}
+  created() {
+    this.getLunbotu();
+  },
+  methods: {
+    async getLunbotu() {
+      const { data: dt } = await this.http.get("/api/getlunbo");
+      console.log(dt);
+      this.Lunbolist = dt.message;
+    }
+  }
 };
 </script>
 
 
-<style>
+<style lang='scss' scoped>
+.mint-swipe {
+  height: 200px;
+  .mint-swipe-item {
+    img {
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
 </style>
 
--- src/main.js
+++ src/main.js
@@ -5,13 +5,20 @@ import App from './App'
 import router from './router'
 // 引入mint-ui
 import MintUI from 'mint-ui'
+// 引入axios
+import axios from 'axios'
 import 'mint-ui/lib/style.css'
 // 引入mui相关资源
-
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 
+import VueResource from 'vue-resource'
+
 Vue.use(MintUI)
+Vue.use(VueResource)
+// 给axios做配置
+axios.defaults.baseURL = 'http://www.liulongbin.top:3005'
+Vue.prototype.http = axios
 
 Vue.config.productionTip = false
 
